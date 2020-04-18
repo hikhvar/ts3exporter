b5acfc6af726d5343a4eef9b33992c23c4ca01dd
Christoph Petrausch <christoph.petrausch@inovex.de>
2020-04-18 13:33:12 +0200

    Support reading the password from the environment

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	remote := flag.String("remote", "localhost:10011", "remote address of server query port")
 	listen := flag.String("listen", ":9189", "listen address of the exporter")
 	user := flag.String("user", "serveradmin", "the serverquery user of the ts3exporter")
-	passwordFile := flag.String("passwordfile", "/etc/ts3exporter/password", "file containing the password. Must have 0600 permission. The file is not read if the environ")
+	passwordFile := flag.String("passwordfile", "/etc/ts3exporter/password", "file containing the password. Must have 0600 permission. The file is not read if the environment variable SERVERQUERY_PASSWORD is set.")
 	enableChannelMetrics := flag.Bool("enablechannelmetrics", false, "Enables the channel collector.")
 	ignoreFloodLimits := flag.Bool("ignorefloodlimits", false, "Disable the server query flood limiter. Use this only if your exporter is whitelisted in the query_ip_whitelist.txt file.")
 
@@ -44,16 +44,19 @@ func main() {
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
 
-// mustReadPassword reads the password from the password file. The password file is only
-// used if password is the empty string.
+// mustReadPassword reads the password from the password file or the environment. The password file is only
+// used if the SERVERQUERY_PASSWORD environment variable is not set.
 // If the file read fails, this function terminates the process.
 func mustReadPassword(passwordFile string) string {
+	if pw, found := os.LookupEnv("SERVERQUERY_PASSWORD"); found {
+		return pw
+	}
 	fInfo, err := os.Stat(passwordFile)
 	if err != nil {
 		log.Fatalf("failed to get fileinfo of password file: %v\n", err)
 	}
-	if fInfo.Mode() != 0600 {
-		log.Fatalf("password file permissions are to open. Have: %s, want: %o\n", fInfo.Mode().String(), 0600)
+	if fInfo.Mode() != 0600 || fInfo.Mode() != 0400 {
+		log.Fatalf("password file permissions are to open. Have: %s, want at most: %o\n", fInfo.Mode().String(), 0600)
 	}
 	data, err := ioutil.ReadFile(passwordFile)
 	if err != nil {
